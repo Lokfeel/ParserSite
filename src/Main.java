055a09a6bcc99523c77aa49b6b08111f24942818
Misha <golovackii@gmail.com>
2021-04-17 20:03:35 +0300

    Parsing site and saving in file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
+        String pathDownload = "G:\\download\\";
+        try {
+            Document doc = Jsoup.connect("https://www.onliner.by/").get();
+            Elements elements = doc.select("img");
+            int numName = 0;
+            for(Element elem : elements){
+                try {
+                    String temp = elem.attr("data-src");
+                    URL url = new URL(temp);
+                    InputStream in = new BufferedInputStream(url.openStream(), 200);
+                    OutputStream out = new BufferedOutputStream(new FileOutputStream(pathDownload + numName + ".jpg"));
+                    for(int i; (i = in.read()) != -1;){
+                        out.write(i);
+                    }
+                    in.close();
+                    out.close();
+                    numName++;
+                }
+                catch (MalformedURLException ex){
+                    ex.getMessage();
+                }
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
 
-        
     }
 }
